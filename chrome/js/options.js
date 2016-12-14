61e5284b10a0cddd2cc6203a4bb17a9c132a126a
Ye Feng <1264669423@qq.com>
2016-12-14 16:53:39 +0800

    Add files via upload

--- chrome/js/options.js
+++ chrome/js/options.js
@@ -8,12 +8,17 @@ var bgp          = chrome.extension.getBackgroundPage();
 var tempHTML     = '<li class="tabel-item bilibili-color1">视频av号</li><li class="tabel-item bilibili-color2">flv</li><li class="tabel-item bilibili-color3">mp4</li>';
 var videoList    = document.getElementById('videoList');
 var clearHistory = document.getElementById('clearHistory');
+var getHelp      = document.getElementById('getHelp');
 
 getVideoList();
 
 clearHistory.onclick = function() {
     bgp.clearVideoList();
     getVideoList();
+};
+
+getHelp.onclick = function() {
+    window.open('http://www.zhihu.com/people/ineer');
 };
 
 bgp.isDownload = true;
